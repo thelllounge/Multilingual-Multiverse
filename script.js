0966e59af94eb7c3b00dbf186c93abe429047aa3
thelllounge <llearninglounge@gmail.com>
2021-11-06 01:16:20 +0900

    add checkboxes fail at hiding things w javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(hello);
+function showHideEnglish() {
+    var en = document.getElementsByClassName('en')
+    var english = document.getElementById('english')
+    if(english.checked == true){
+        en.style.display = "block";
+    }else{
+        en.style.display = "none";
+    }
+}
