719c7065e276217f21dc8854c411dc76a138e24d
thelllounge <llearninglounge@gmail.com>
2021-11-08 17:48:38 +0900

    add json file, attempt to create table, fail

--- script.js
+++ script.js
@@ -31,7 +31,45 @@
     $('#chineseSimp').on('click', function() {
         $(".ch").toggleClass('hidden');
     });
+// One attempt
+    // const data = fetch('./MtGTrial.json');
+
  
-        
+    // for (var i in data){
+    //     var row =   `<tr>
+    //                          <td> ${data[i].English} </td>
+    //                          <td>${data[i].French}</td>
+    //                          <td>${data[i].Spanish}</td>
+    //                          <td>${data[i].Italian}</td>
+    //                          <td>${data[i].Portuguese}</td>
+    //                          <td>${data[i].German}</td>
+    //                          <td>${data[i].Japanese}</td>
+    //                          <td>${data[i].SimplifiedChinese}</td>
+    //                      </tr>`
+    //          var table = $('#vocabBody')
+    //          table.append(row);
+    // }
+
+// Another attempt
+
+    // function buildTable(data){
+    //     var table = document.getElementById('vocabBody')
+    //     for (var i = 0; i < data.length; i++){
+    //         var row =   `<tr>
+    //                         <td> ${data[i].English} </td>
+    //                         <td>${data[i].French}</td>
+    //                         <td>${data[i].Spanish}</td>
+    //                         <td>${data[i].Italian}</td>
+    //                         <td>${data[i].Portuguese}</td>
+    //                         <td>${data[i].German}</td>
+    //                         <td>${data[i].Japanese}</td>
+    //                         <td>${data[i].SimplifiedChinese}</td>
+    //                     </tr>`
+    //         table.append(row);
+    //     }
+    // };
+
+    // buildTable("Magic Memrise Trial.json");
+
 
 });
