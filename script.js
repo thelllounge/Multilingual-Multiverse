e2adf17a289b6da9c33cb72526332cc7d5791493
thelllounge <llearninglounge@gmail.com>
2021-11-08 20:51:06 +0900

    continute to fail at json to html table

--- script.js
+++ script.js
@@ -31,45 +31,53 @@
     $('#chineseSimp').on('click', function() {
         $(".ch").toggleClass('hidden');
     });
+
+
+// Making a table   
+
+    let tableData = fetch('MtGTrial.json')
+        .then(Response => Response.json())
+        .then(json => console.log(json));
 // One attempt
-    // const data = fetch('./MtGTrial.json');
 
  
-    // for (var i in data){
-    //     var row =   `<tr>
-    //                          <td> ${data[i].English} </td>
-    //                          <td>${data[i].French}</td>
-    //                          <td>${data[i].Spanish}</td>
-    //                          <td>${data[i].Italian}</td>
-    //                          <td>${data[i].Portuguese}</td>
-    //                          <td>${data[i].German}</td>
-    //                          <td>${data[i].Japanese}</td>
-    //                          <td>${data[i].SimplifiedChinese}</td>
-    //                      </tr>`
+    // for (var i in tableData){
+    //     var row =  `<tr>
+    //                     <td>${tableData[i].English}</td>
+    //                     <td>${tableData[i].French}</td>
+    //                     <td>${tableData[i].Spanish}</td>
+    //                     <td>${tableData[i].Italian}</td>
+    //                     <td>${tableData[i].Portuguese}</td>
+    //                     <td>${tableData[i].German}</td>
+    //                     <td>${tableData[i].Japanese}</td>
+    //                     <td>${tableData[i].SimplifiedChinese}</td>
+    //                 </tr>`
     //          var table = $('#vocabBody')
     //          table.append(row);
     // }
 
 // Another attempt
 
-    // function buildTable(data){
-    //     var table = document.getElementById('vocabBody')
-    //     for (var i = 0; i < data.length; i++){
-    //         var row =   `<tr>
-    //                         <td> ${data[i].English} </td>
-    //                         <td>${data[i].French}</td>
-    //                         <td>${data[i].Spanish}</td>
-    //                         <td>${data[i].Italian}</td>
-    //                         <td>${data[i].Portuguese}</td>
-    //                         <td>${data[i].German}</td>
-    //                         <td>${data[i].Japanese}</td>
-    //                         <td>${data[i].SimplifiedChinese}</td>
-    //                     </tr>`
-    //         table.append(row);
-    //     }
-    // };
+    buildTable(tableData);
 
-    // buildTable("Magic Memrise Trial.json");
+    function buildTable(data){
+        var table = document.getElementById('vocabBody')
+        for (var i = 0; i < data.length; i++){
+            var row =   `<tr>
+                            <td>${data[i].English}</td>
+                            <td>${data[i].French}</td>
+                            <td>${data[i].Spanish}</td>
+                            <td>${data[i].Italian}</td>
+                            <td>${data[i].Portuguese}</td>
+                            <td>${data[i].German}</td>
+                            <td>${data[i].Japanese}</td>
+                            <td>${data[i].SimplifiedChinese}</td>
+                        </tr>`
+            table.innterHTML += row
+        }
+    };
+
+
 
 
 });
